d3d95cca7c87a69cde7f3ecfcaf51716cd5dcbd7
Sebastian Rehnby <sebastian@rehnby.com>
2015-03-22 14:04:19 +0100

    Fixed retain semantics on DDCometSubscription

--- DDComet/DDCometSubscription.h
+++ DDComet/DDCometSubscription.h
@@ -4,8 +4,8 @@
 
 @interface DDCometSubscription : NSObject
 
-@property (nonatomic, readonly) NSString *channel;
-@property (nonatomic, readonly) id target;
+@property (nonatomic, copy, readonly) NSString *channel;
+@property (nonatomic, unsafe_unretained, readonly) id target;
 @property (nonatomic, readonly) SEL selector;
 
 - (id)initWithChannel:(NSString *)channel target:(id)target selector:(SEL)selector;
